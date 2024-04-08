deca94d5382f2c6a482472d1a2241a9e7e0e8ca0
Tanishka Gupta <tanishkagupta2207@gmail.com>
2024-04-09 03:06:28 +0530

    login UI

--- src/views/login.js
+++ src/views/login.js
@@ -1,127 +1,119 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './login.css'; 
+import { useNavigate } from 'react-router-dom';
 
-const LoginForm = () => {
+const Login = () => {
     const [formData, setFormData] = useState({
-        emailId: '',
-        password: ''
-    });
+                emailId: '',
+                password: ''
+            });
+        
+            const navigate = useNavigate(); 
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:5000/api/login', formData);
-            console.log(response.data);
-            if(response.message){
-                alert(response.message);
-            }
-        } catch (error) {
-            console.error('Login error:', error.response.data);
-        }
-    };
+            const handleChange = (e) => {
+                setFormData({ ...formData, [e.target.name]: e.target.value });
+            };
+        
+            const handleSubmit = async (e) => {
+                e.preventDefault();
+                try {
+                    const response = await axios.post('http://localhost:5000/api/login', formData);
+                    console.log(response.data);
+                    if(response.data.message){
+                        alert(response.data.message);
+                        if(response.data.message !== 'Logged in successfully'){
+                        window.location.reload();
+                        }
+                        else{
+                            navigate(`/form1/${response.data.userId}`);
+                        }
+                    }
+                } catch (error) {
+                    console.error('Login error:', error.response.data);
+                }
+            };
     return (
-        // <div>
-        //     <div class="container-fluid" style={{backgroundColor: '#f7ffff', marginBottom: '10px'}}>
-        //         <div class="container">
-        //             <div class="row" style={{marginBottom:'10px' }}>
-        //                 <div class="col-md-8 col-md-offset-2">
-        //                     <h3 style={{textAlign:'center',color:'#414002!important',fontWeight: 'bold',fontSize: '2.3em',marginTop: '3px',fontFamily: 'Noto Sans'}}>भारतीय प्रौद्योगिकी संस्थान पटना </h3>
-        //                     <h3 style={{textAlign:'center',color: '#414002!important',fontWeight:'bold',fontFamily: 'Oswald',fontSize: '2.2em', marginTop: '0px'}}>Indian Institute of Technology Patna</h3>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div> 
-        //     <h3 style={{color: '#e10425', marginBottom: '20px', fontWeight: 'bold', textAlign: 'center',fontFamily: 'Noto Serif'}} class="blink_me">Application for Faculty Position</h3>
+        <html>
+            <head>
+                <title>Faculty Login</title>
+                <link rel="stylesheet" type="text/css" href="https://ofa.iiti.ac.in/facrec_che_2023_july_02/images/favicon.ico"/>
+                <link rel="icon" href="https://ofa.iiti.ac.in/facrec_che_2023_july_02/images/favicon.ico" type="image/x-icon" />
+                <link rel="stylesheet" type="text/css" href="https://ofa.iiti.ac.in/facrec_che_2023_july_02/css/bootstrap.css" />
+                <link rel="stylesheet" type="text/css" href="https://ofa.iiti.ac.in/facrec_che_2023_july_02/css/bootstrap-datepicker.css" />
+                <link href="https://fonts.googleapis.com/css?family=Sintony" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet" />
+            </head>
+            <body className="login-page">
+                <div className="container-fluid" style={{ backgroundColor: '#f7ffff', marginBottom: '10px' }}>
+                    <div className="container">
+                        <div className="row" style={{ marginBottom: '10px' }}>
+                            <div className="col-md-8 col-md-offset-2">
+                                <h3 className="title">भारतीय प्रौद्योगिकी संस्थान पटना</h3>
+                                <h3 className="subtitle">Indian Institute of Technology Patna</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <h3 className="application-title blink_me">Application for Faculty Position</h3>
 
-        //     <div class="container" style= {{borderRadius:'10px', marginTop:'20px'}}>
-        //         <div class="row" style={{borderWidth: '2px', borderStyle: 'solid', borderRadius: '10px', boxShadow: '0px 1px 30px 1px #284d7a', height:'500px', backgroundColor:'#F7FFFF'}}>
-        //             <div class="col-md-6 col-sm-6 col-xs-6">
-        //                 <img alt="College Logo" src="https://ofa.iiti.ac.in/facrec_che_2023_july_02/images/IITIndorelogo.png" style={{marginLeft:'22%', marginTop: '5%'}}></img>
-        //             </div>
-        //             <div class="col-xs-12 col-sm-12 col-md-6">
-        //                 <h3>CREATE YOUR PROFILE</h3><br />
-        //                 <form action="#" method="post" class="form">
-        //                     <input type="hidden" name="ci_csrf_token" value="" />
-        //                     <div class="row">
-        //                         <div class="col-xs-6 col-md-6">
-        //                             <input class="form-control" value='' name="firstname" placeholder="First name" type="text"
-        //                                 required="" autofocus />
-        //                         </div>
-        //                         <div class="col-xs-6 col-md-6">
-        //                             <input class="form-control" name="lastname"  value='' required="" placeholder="Last name" type="text" />
-        //                         </div>
-        //                     </div>
-        //                     <div class="row">
-        //                         <div class="col-xs-6 col-md-6">
-        //                             <input class="form-control" name="email" placeholder="Your email"  value='' required="" type="email" />
-        //                         </div>
-        //                             <div class="col-xs-6 col-md-6">
-        //                                 <select id="cast" name="cast" class="form-control input-md" required="">
-        //                                     <option value="">Select Category</option>
-        //                                     <option value="UR">UR</option>
-        //                                     <option value="OBC">OBC</option>
-        //                                     <option value="SC">SC</option>
-        //                                     <option value="ST">ST</option>
-        //                                     <option value="PWD">PWD</option>
-        //                                     <option value="EWS">EWS</option>
-        //                                 </select>
-        //                             </div>
-        //                     </div>
-        //                     <div class="row">
-        //                         <div class="col-md-6 col-sm-6 col-xs-6">
-        //                             <input class="form-control" name="password" placeholder="New password" required="" type="password" />
-        //                         </div>
-        //                         <div class="col-md-6 col-sm-6 col-xs-6">
-        //                             <input class="form-control" name="re_password" placeholder="Retype - new password" required="" type="password" />
-        //                         </div>
-        //                     </div>
-        //                     <div class="row">
-        //                             <input id="created_at" name="created_at" type="hidden"/>
-        //                             <div class="col-xs-6 col-md-6">
-        //                                 <img src="https://ofa.iiti.ac.in/facrec_che_2023_july_02/viewcaptcha?t=0.50918900+1712492032" alt="CAPTCHA security code" />
-        //                             </div>
-        //                         <div class="col-xs-6 col-md-6" >
-        //                             <input class="form-control" name="captcha" placeholder="Insert image text" required="" type="text" />
-        //                         </div>
-        //                         <div class="col-xs-12 col-md-12">
-        //                             <h5><strong><font color="red">Note:</font>
-        //                             <br />  
-        //                             <br />  
-        //                             <br />
-        //                             <font color='#124f93'>
-        //                             1. Applicant should kindly check their email for activation link to access the portal. 
-        //                             <br />  
-        //                             2. Please check SPAM folder also, in case activation link is not received in INBOX.<br />
-        //                             3. Applicant applying for more than one position/ department should use <strong><font color="red">different email id</font></strong> for each application.</font> </strong>
-        //                             <br />
-        //                             <br />
-        //                             <br />
-        //                             </h5>
-        //                             <button class="btn btn-sm btn-primary" type="submit" name="submit" value="Submit">Register</button>
-        //                             <strong class=" pull-right" style={{color: 'green'}}>If registered <a href='https://ofa.iiti.ac.in/facrec_che_2023_july_02/faculty/login' class="btn btn-sm btn-success"> Login Here</a></strong>
-        //                         </div>
-        //                     </div>
+                <div className="container" style={{ borderRadius: '10px', height: '300px', marginTop: '20px' }}>
+                    <div className="col-md-10 col-md-offset-1">
+                        <div className="row" style={{ borderWidth: '2px', borderStyle: 'solid', borderRadius: '10px', boxShadow: '0px 1px 30px 1px #284d7a', backgroundColor: '#F7FFFF' }}>
+                            <div className="col-md-6" style={{ height: '403px', borderRadius: '10px 0px 0px 10px' }}>
+                                <img src="https://www.pngkit.com/png/detail/270-2702179_iit-patna-logo-png.png" style={{ marginTop: '5%', marginLeft: '20%', height: '75%' }} alt="IIT Indore Logo" />
+                            </div>
+                            <div className="col-md-6" style={{ borderRadius: '0px 10px 10px 0px', height: '403px' }}>
+                                <br />
+                                <div className="col-md-10 col-md-offset-1">
+                                    <h3 style={{ textAlign: 'center' }}><strong><u>LOGIN HERE</u></strong></h3>
+                                    <br />
+                                    <form method="post">
+                                        <input type="hidden" name="ci_csrf_token" value="" />
 
-        //                 </form>
+                                        <div className="inner-addon left-addon">
+                                            <i className="glyphicon glyphicon-envelope"></i>
+                                            <input type="text" name="emailId" placeholder="Your email" autoFocus required onChange={handleChange}/>
+                                        </div>
+                                        <br />
 
-        //             </div>
-        //         </div>
+                                        <div className="inner-addon left-addon">
+                                            <i className="glyphicon glyphicon-lock"></i>
+                                            <input type="password" placeholder="Enter your password" name="password" required onChange={handleChange} />
+                                        </div>
+                                        <br />
 
-        //     </div>
-        // </div>
-        <form onSubmit={handleSubmit}>
-            <input type="email" name="emailId" placeholder="Email" onChange={handleChange} />
-            <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-            <button type="submit">Register</button>
-        </form>
+                                        <div className="row">
+                                            <div className="col-md-3">
+                                                <button type="submit" name="submit" value="Submit" onClick={handleSubmit}>Login</button>
+                                            </div>
+                                            <div className="col-md-9">
+                                                <a href="https://ofa.iiti.ac.in/facrec_che_2023_july_02/faculty/forgot_password">
+                                                    <button type="button" className="cancelbtn pull-right">Reset Password</button>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <br />
+                                    <p style={{ textAlign: 'center', color: 'green', fontSize: '1.3em' }}>
+                                        <strong>NOT REGISTERED? </strong> <a href='http://localhost:3000/register' className="btn-sm btn-primary"> SIGN UP</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="footer"></div>
+            </body>
+        </html>
     );
 };
 
-export default LoginForm;
-
-
+export default Login;
